17f64c8c4e3e4702fc63af4322bed4073db65ce4
27Lia <tjs4114@gmail.com>
2023-10-03 04:13:47 +0900

    feat:로그인기능구현

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -2,48 +2,46 @@ import styled from "styled-components";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth"; // 여기서 signInWithEmailAndPassword 함수를 직접 import
 
-// 로그인 페이지 전체를 감싸는 컨테이너를 스타일링합니다.
 const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
+  gap: 15px;
 `;
 
-// 입력 필드를 스타일링합니다.
 const StyledInput = styled.input`
   margin-bottom: 10px;
-  padding: 10px;
+  padding: 15px;
   border-radius: 5px;
   border: 1px solid #ddd;
-  width: 200px;
+  width: 300px;
+  font-size: 20px;
 `;
 
-// 로그인 버튼을 스타일링합니다.
 const StyledButton = styled.button`
-  padding: 10px 20px;
-  background-color: #28a745;
+  padding: 10px 30px;
+  background-color: #412dd4;
   color: #fff;
   border: none;
   border-radius: 5px;
   cursor: pointer;
   &:hover {
-    background-color: #218838;
+    background-color: #5b4adb;
   }
 `;
 
 function LoginPage() {
-  console.log("LoginPage 렌더링");
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
-      await auth.signInWithEmailAndPassword(email, password);
+      await signInWithEmailAndPassword(auth, email, password);
       navigate("/");
     } catch (error) {
       console.error("로그인 에러:", error);
