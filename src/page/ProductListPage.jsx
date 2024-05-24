9d74105eac6620d44f6a8fe1af66e0e2b3aa711f
27Lia <117743861+27Lia@users.noreply.github.com>
2024-05-24 13:30:57 +0900

    -

--- src/page/ProductListPage.jsx
+++ src/page/ProductListPage.jsx
@@ -5,9 +5,10 @@ import InnerContainer from "./InnerContainer";
 import Nav from "../component/Nav";
 import { useSelector } from "react-redux";
 import { db, auth } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 import { fetchProducts } from "../redux";
 import { useDispatch } from "react-redux";
+import { useInView } from "react-intersection-observer";
 
 const StyleProductList = styled.div`
   nav {
@@ -43,41 +44,63 @@ const StyleProductList = styled.div`
 function ProductListPage() {
   const [filterOption, setFilterOption] = useState("전체");
   const products = useSelector((state) => state.products);
+
   const [filteredProducts, setFilteredProducts] = useState([]);
   const dispatch = useDispatch();
+
+  const [ref, inView] = useInView({
+    threshold: 0,
+    triggerOnce: false,
+  });
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
   useEffect(() => {
-    const filterProducts = async (user) => {
-      let bookmarkedIds = [];
+    if (inView) {
+      dispatch(fetchProducts());
+    }
+  }, [inView, dispatch]);
+
+  useEffect(() => {
+    const unsubscribeAuth = auth.onAuthStateChanged((user) => {
       if (user) {
         const userDocRef = doc(db, "users", user.uid);
-        const docSnap = await getDoc(userDocRef);
 
-        if (docSnap.exists()) {
-          const userData = docSnap.data();
-          if (userData.bookmarks) {
-            bookmarkedIds = userData.bookmarks.map((bookmark) => bookmark.id);
+        // onSnapshot을 사용하여 북마크 정보의 실시간 변경을 감시
+        const unsubscribeSnapshot = onSnapshot(userDocRef, (docSnap) => {
+          let bookmarkedIds = [];
+          if (docSnap.exists()) {
+            const userData = docSnap.data();
+            if (userData.bookmarks) {
+              // bookmarks가 존재하는지 확인
+              bookmarkedIds = userData.bookmarks.map((bookmark) => bookmark.id);
+            }
           }
-        }
+
+          const nonBookmarkedFilteredProducts = products.filter(
+            (product) =>
+              !bookmarkedIds.includes(product.id) &&
+              (filterOption === "전체" || product.type === filterOption)
+          );
+
+          setFilteredProducts(nonBookmarkedFilteredProducts);
+        });
+
+        // 컴포넌트 언마운트 시 onSnapshot 감시 중지
+        return () => unsubscribeSnapshot();
+      } else {
+        // 사용자가 로그인하지 않은 경우에도 필터링을 적용하여 상품을 표시
+        const nonBookmarkedFilteredProducts = products.filter(
+          (product) => filterOption === "전체" || product.type === filterOption
+        );
+
+        setFilteredProducts(nonBookmarkedFilteredProducts);
       }
-
-      const nonBookmarkedFilteredProducts = products.filter(
-        (product) =>
-          !bookmarkedIds.includes(product.id) &&
-          (filterOption === "전체" || product.type === filterOption)
-      );
-
-      setFilteredProducts(nonBookmarkedFilteredProducts);
-    };
-
-    const unsubscribeAuth = auth.onAuthStateChanged((user) => {
-      filterProducts(user);
     });
 
+    // 인증 상태 리스너 해제
     return () => unsubscribeAuth();
   }, [products, filterOption]);
 
@@ -89,6 +112,7 @@ function ProductListPage() {
           {filteredProducts.map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
+          <div className="blank" ref={ref} />
         </main>
       </StyleProductList>
     </InnerContainer>
